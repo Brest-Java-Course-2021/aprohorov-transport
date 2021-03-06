35fc99cbcb3056e20df944ee24002d53e568e869
Artsiom Prokharau <artem.prohoroff@gmail.com>
2021-03-06 17:19:15 +0300

    fix dao and created config class Spring

---
+++ dao/src/main/java/by/prohor/dao/config/DaoConfiguration.java
@@ -0,0 +1,8 @@
+package by.prohor.dao.config;
+
+/**
+ * Created by Artsiom Prokharau 06.03.2021
+ */
+
+public class DaoConfiguration {
+}
---
+++ service/src/main/java/by/prohor/service/config/ServiceConfiguration.java
@@ -0,0 +1,8 @@
+package by.prohor.service.config;
+
+/**
+ * Created by Artsiom Prokharau 06.03.2021
+ */
+
+public class ServiceConfiguration {
+}
